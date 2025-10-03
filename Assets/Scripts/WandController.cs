76d52e26f0af07e4777cb6d4a17ccef322b3460b
Emily Flauto <eflauto@student.cscc.edu>
2025-10-03 12:34:25 -0400

    Added base wand controller
    Started the base wand controller. It currently just plays the animation when fired.

---
+++ Assets/Scripts/WandController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandController : MonoBehaviour
+{
+    public List<string> attacks;
+    private string _selectedAttack;
+    
+    private Animator _animator;
+    
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    
+    private void Update()
+    {
+        if (!Input.GetButtonDown("Fire1")) return;
+        
+        // 0 is the "Attack" parameter of the wand. You can check these in the animator window.
+        _animator.SetTrigger(0);
+        BasicAttack();
+    }
+
+    private void BasicAttack()
+    {
+        
+    }
+}
