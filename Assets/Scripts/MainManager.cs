0c902d122c656a70179e9533c7a7f3f842993ee5
Emily Flauto <eflauto@student.cscc.edu>
2025-10-10 12:18:57 -0400

    Add variables to main manager
    Added a health variables, the settings variables, and related functions to the main manager script.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,7 +3,20 @@ using UnityEngine;
 public class MainManager : MonoBehaviour
 {
     public static MainManager Instance;
+
+    [Header("Gameplay Variables")] 
+    public int health = 30;
     public int selectedSpell = 0;
+
+    [Header("Settings")] 
+    public bool fpsCap = true;
+    public int fpsTarget = 60;
+    public int musicVolume = 100;
+    public int sfxVolume = 100;
+    public float mouseSensitivityX = 3f;
+    public float mouseSensitivityY = 3f;
+    public bool invertX =  false;
+    public bool invertY = false;
 
     private void Awake()
     {
@@ -20,5 +33,26 @@ public class MainManager : MonoBehaviour
     public void NewSpellSelected(int spellIndex)
     {
         Instance.selectedSpell = spellIndex;
+    }
+
+    public void SetFPSCap(bool setFPSCap)
+    {
+        Instance.fpsCap = setFPSCap;
+
+        if (Instance.fpsCap)
+        {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = Instance.fpsTarget;
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 1;
+        }
+    }
+
+    public void SetFPSTarget(int target)
+    {
+        Instance.fpsTarget = target;
+        Application.targetFrameRate = Instance.fpsTarget;
     }
 }
