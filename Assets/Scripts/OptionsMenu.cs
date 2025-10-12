60ecaffc3d39b079eccb64bb1e55f50d339a8ef7
Emily Flauto <eflauto@student.cscc.edu>
2025-10-12 11:47:31 -0400

    Add persistence for settings
    Added persistence for settings. Now, all settings should persist even when the game is restarted.

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,10 +11,21 @@ public class OptionsMenu : MonoBehaviour
     public Toggle invertYToggle;
     public Slider sensitivitySlider;
     public Slider volumeSlider;
+
+    public void Awake()
+    {
+        ReadSettings();
+    }
     
     public void UpdateFPSCap()
     {
         MainManager.Instance.fpsCap = fpsCapToggle.isOn;
+    }
+
+    private void UpdateFPSCap(bool fpsCap)
+    {
+        MainManager.Instance.fpsCap = fpsCap;
+        fpsCapToggle.isOn = fpsCap;
     }
 
     public void UpdateFPS()
@@ -22,14 +34,33 @@ public class OptionsMenu : MonoBehaviour
         fpsSlider.transform.Find("Value").GetComponent<TextMeshProUGUI>().text = MainManager.Instance.fpsTarget.ToString();
     }
 
+    private void UpdateFPS(int fpsTarget)
+    {
+        MainManager.Instance.fpsTarget = fpsTarget;
+        fpsSlider.value = fpsTarget;
+        fpsSlider.transform.Find("Value").GetComponent<TextMeshProUGUI>().text = MainManager.Instance.fpsTarget.ToString();
+    }
+
     public void UpdateInvertX()
     {
         MainManager.Instance.invertX = invertXToggle.isOn;
     }
 
+    private void UpdateInvertX(bool invertX)
+    {
+        MainManager.Instance.invertX = invertX;
+        invertXToggle.isOn = invertX;
+    }
+
     public void UpdateInvertY()
     {
         MainManager.Instance.invertY = invertYToggle.isOn;
+    }
+
+    private void UpdateInvertY(bool invertY)
+    {
+        MainManager.Instance.invertY = invertY;
+        invertYToggle.isOn = invertY;
     }
 
     public void UpdateSensitivity()
@@ -40,10 +71,53 @@ public class OptionsMenu : MonoBehaviour
             MainManager.Instance.mouseSensitivityX.ToString(".0");
     }
 
+    private void UpdateSensitivity(float sensitivity)
+    {
+        MainManager.Instance.mouseSensitivityX = sensitivity;
+        MainManager.Instance.mouseSensitivityY = sensitivity;
+        sensitivitySlider.value = sensitivity;
+        sensitivitySlider.transform.Find("Value").GetComponent<TextMeshProUGUI>().text = sensitivity.ToString(".0");
+    }
+
     public void UpdateVolume()
     {
         MainManager.Instance.musicVolume = (int)volumeSlider.value;
         MainManager.Instance.sfxVolume = (int)volumeSlider.value;
         volumeSlider.transform.Find("Value").GetComponent<TextMeshProUGUI>().text = MainManager.Instance.musicVolume.ToString();
+    }
+
+    private void UpdateVolume(int volume)
+    {
+        MainManager.Instance.musicVolume = volume;
+        MainManager.Instance.sfxVolume = volume;
+        volumeSlider.value = MainManager.Instance.musicVolume;
+        volumeSlider.transform.Find("Value").GetComponent<TextMeshProUGUI>().text = volume.ToString();
+    }
+
+    public void ReadSettings()
+    {
+        var fpsCap = PlayerPrefs.GetInt("FPSCap", 1);
+        var fpsTarget = PlayerPrefs.GetInt("FPSTarget", 60);
+        var invertX = PlayerPrefs.GetInt("InvertX", 0);
+        var invertY = PlayerPrefs.GetInt("InvertY", 0);
+        var sensitivity = PlayerPrefs.GetFloat("Sensitivity", 2.0f);
+        var volume = PlayerPrefs.GetInt("Volume", 100);
+        
+        UpdateFPSCap(Convert.ToBoolean(fpsCap));
+        UpdateFPS(fpsTarget);
+        UpdateInvertX(Convert.ToBoolean(invertX));
+        UpdateInvertY(Convert.ToBoolean(invertY));
+        UpdateSensitivity(sensitivity);
+        UpdateVolume(volume);
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("FPSCap", MainManager.Instance.fpsCap ? 1 : 0);
+        PlayerPrefs.SetInt("FPSTarget", MainManager.Instance.fpsTarget);
+        PlayerPrefs.SetInt("InvertX", MainManager.Instance.invertX ? 1 : 0);
+        PlayerPrefs.SetInt("InvertY", MainManager.Instance.invertY ? 1 : 0);
+        PlayerPrefs.SetFloat("Sensitivity", MainManager.Instance.mouseSensitivityX);
+        PlayerPrefs.SetInt("Volume", MainManager.Instance.musicVolume);
     }
 }
