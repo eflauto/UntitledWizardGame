8b2a9c2476f81c16cec85891468752ee9c217f77
Emily Flauto <eflauto@student.cscc.edu>
2025-10-11 22:32:23 -0400

    Add menu code
    Added the menu code but didn't link it up to the menu buttons yet.

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public Toggle fpsCapToggle;
+    public Slider fpsSlider;
+    public Toggle invertXToggle;
+    public Toggle invertYToggle;
+    public Slider sensitivitySlider;
+    public Slider volumeSlider;
+    
+    public void UpdateFPSCap()
+    {
+        MainManager.Instance.fpsCap = fpsCapToggle.isOn;
+    }
+
+    public void UpdateFPS()
+    {
+        MainManager.Instance.fpsTarget = (int)fpsSlider.value;
+        fpsSlider.transform.Find("Value").GetComponent<Text>().text = MainManager.Instance.fpsTarget.ToString();
+    }
+
+    public void UpdateInvertX()
+    {
+        MainManager.Instance.invertX = invertXToggle.isOn;
+    }
+
+    public void UpdateInvertY()
+    {
+        MainManager.Instance.invertY = invertYToggle.isOn;
+    }
+
+    public void UpdateSensitivity()
+    {
+        MainManager.Instance.mouseSensitivityX =  sensitivitySlider.value;
+        MainManager.Instance.mouseSensitivityY = sensitivitySlider.value;
+        sensitivitySlider.transform.Find("Value").GetComponent<Text>().text =
+            MainManager.Instance.mouseSensitivityX.ToString();
+    }
+
+    public void UpdateVolume()
+    {
+        MainManager.Instance.musicVolume = (int)volumeSlider.value;
+        MainManager.Instance.sfxVolume = (int)volumeSlider.value;
+        volumeSlider.transform.Find("Value").GetComponent<Text>().text = MainManager.Instance.musicVolume.ToString();
+    }
+}
