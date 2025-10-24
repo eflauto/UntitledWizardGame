42d47a4e6ef8b38c0ec907d5437398d1c3ed58d8
jo-mos <jomos.dev@pm.me>
2025-10-23 20:44:20 -0400

    Fire Spell Fix 1
    fixed/tweaked some values to make sure the spell fires in the direction it's supposed to

--- Assets/Scripts/FireBreathSpell.cs
+++ Assets/Scripts/FireBreathSpell.cs
@@ -15,12 +15,13 @@ public class FireBreathSpell : Attack
     public void FireBreath(Transform callerTransform)
     {
         var instance = Instantiate(fireBreathObject, callerTransform.position, callerTransform.rotation);
-        var fwd = callerTransform.TransformDirection(Vector3.forward);
-        var fireSpawnPosition = fwd * 1.5f + callerTransform.position;
+        var fwd = callerTransform.TransformDirection(Vector3.back); // this use of .back will send the boxcast properly forward   
+        var fireSpawnPosition = fwd * 1.1f + callerTransform.position;
         var fireBlastedHits = Physics.BoxCastAll(fireSpawnPosition, new Vector3(1, .5f, 1), fwd, callerTransform.rotation, fireDistance, layerMask);
 
         foreach (var rayHit in fireBlastedHits)
         {
+            //Debug.Log(rayHit.collider.gameObject.name);
             if (!rayHit.collider.gameObject.CompareTag("Enemy") && rayHit.collider.gameObject.name != "Web") return;
 
             if (rayHit.collider.gameObject.name == "Web")
