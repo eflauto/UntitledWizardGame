49c5d51e8470d9a1945c9bd515781397846ce1b6
Emily Flauto <eflauto@student.cscc.edu>
2025-10-27 12:27:23 -0400

    Add feedback on spell pickup
    Added some feedback on spell pickup. Now there is text that says "new spell" when you pick up a spell. Hopefully this points people in the right direction.

--- Assets/Scripts/SpellPickupScript.cs
+++ Assets/Scripts/SpellPickupScript.cs
@@ -1,12 +1,75 @@
+using System;
+using TMPro;
 using UnityEngine;
+using System.Collections;
 
 public class SpellPickupScript : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    private TextMeshProUGUI _newSpellText;
+    private Color _originalTextColor;
+
+    private void Start()
+    {
+        _newSpellText = GameObject.Find("UI").transform.Find("NewSpell").GetComponent<TextMeshProUGUI>();
+        _originalTextColor =  _newSpellText.color;
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
         
         MainManager.Instance.unlockedSpellsCount += 1;
+        transform.localScale = Vector3.zero;
+        
+        StartCoroutine(ChangeTextOpacityCoroutine(1f));
+        StartCoroutine(DelayedChangeTextOpacityCoroutine(0f, 4f));
+        StartCoroutine(DestroyObjectCoroutine(7f));
+    }
+
+    private IEnumerator ChangeTextOpacityCoroutine(float targetOpacity)
+    {
+        var fadingColor = _newSpellText.color;
+        const float endTime = 2f;
+        var timer = 0f;
+
+        while (timer < endTime)
+        {
+            fadingColor.a = Mathf.Lerp(fadingColor.a, targetOpacity, timer / endTime);
+            _newSpellText.color = fadingColor;
+            timer += Time.deltaTime;
+            
+            yield return null;
+        }
+        
+        fadingColor.a = targetOpacity;
+        _newSpellText.color = fadingColor;
+    }
+
+    private IEnumerator DelayedChangeTextOpacityCoroutine(float targetOpacity, float delay)
+    {
+        var timer = 0f;
+
+        while (timer < delay)
+        {
+            timer += Time.deltaTime;
+
+            yield return null;
+        }
+        
+        StartCoroutine(ChangeTextOpacityCoroutine(targetOpacity));
+    }
+
+    private IEnumerator DestroyObjectCoroutine(float delay)
+    {
+        var timer = 0f;
+
+        while (timer < delay)
+        {
+            timer += Time.deltaTime;
+            
+            yield return null;
+        }
+        
         Destroy(gameObject);
     }
 }
